a90b43422131ae500fc9b76baf0e45b41e0c781c
Defenex <defenex@chandlerbade.com>
2020-12-04 17:09:09 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# That Huge
+
+A simple WoW addon that responds to guildmates' achievements with "GRATS THAT HUGE"
+
+## Description
+
+Sends a message saying "GRATS THAT HUGE" in guild chat when someone in the guild gets an achievement.
---
+++ ThatHuge.lua
@@ -0,0 +1,15 @@
+local ThatHugeFrame = CreateFrame("frame", "ThatHugeFrame")
+
+ThatHugeFrame:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT")
+
+lastTimeTriggered = 0
+
+ThatHugeFrame:SetScript("OnEvent", function(self, event, ...)
+    if(event == "CHAT_MSG_GUILD_ACHIEVEMENT") then
+        local seconds = GetTime()
+        if(seconds - lastTimeTriggered > 1) then
+            SendChatMessage("GRATS THAT HUGE", "GUILD", nil)
+            lastTimeTriggered = seconds
+        end
+    end
+end)
